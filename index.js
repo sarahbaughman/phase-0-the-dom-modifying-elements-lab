820fc506ea130708b79051b87dbd9f942bdfad1d
Sarah Baughman <sarahbaughman92@gmail.com>
2023-03-22 09:49:13 -0400

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+main.remove();
+const newHeader=document.createElement("h1")
+newHeader.setAttribute('id','victory');
+newHeader.innerHTML = 'Sarah is the champion';
+document.body.append(newHeader);
+
+
+
